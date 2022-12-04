69da5c4f42471660da6b310ef8e37640a236fb75
Sraavya Pradeep <46724697+SraavyaPradeep@users.noreply.github.com>
2022-12-04 15:28:06 -0800

    Add files via upload

---
+++ M6/M6.ino
@@ -0,0 +1,84 @@
+#include <ArduinoBLE.h>
+#include <Arduino_LSM6DS3.h>
+
+#define BLE_UUID_ACCELEROMETER_SERVICE "1101"
+#define BLE_UUID_ACCELEROMETER_X "2101"
+#define BLE_UUID_ACCELEROMETER_Y "2102"
+#define BLE_UUID_ACCELEROMETER_Z "2103"
+
+#define BLE_DEVICE_NAME "Elfo"
+#define BLE_LOCAL_NAME "Elfo"
+
+BLEService accelerometerService(BLE_UUID_ACCELEROMETER_SERVICE);
+
+BLEFloatCharacteristic accelerometerCharacteristicX(BLE_UUID_ACCELEROMETER_X, BLERead | BLENotify);
+BLEFloatCharacteristic accelerometerCharacteristicY(BLE_UUID_ACCELEROMETER_Y, BLERead | BLENotify);
+BLEFloatCharacteristic accelerometerCharacteristicZ(BLE_UUID_ACCELEROMETER_Z, BLERead | BLENotify);
+
+float x, y, z;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // initialize IMU
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1)
+      ;
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println("Hz");
+
+  // initialize BLE
+  if (!BLE.begin()) {
+    Serial.println("Starting Bluetooth® Low Energy module failed!");
+    while (1)
+      ;
+  }
+
+  // set advertised local name and service UUID
+  BLE.setDeviceName(BLE_DEVICE_NAME);
+  BLE.setLocalName(BLE_LOCAL_NAME);
+  BLE.setAdvertisedService(accelerometerService);
+
+  accelerometerService.addCharacteristic(accelerometerCharacteristicX);
+  accelerometerService.addCharacteristic(accelerometerCharacteristicY);
+  accelerometerService.addCharacteristic(accelerometerCharacteristicZ);
+
+  BLE.addService(accelerometerService);
+
+  accelerometerCharacteristicX.writeValue(0);
+  accelerometerCharacteristicY.writeValue(0);
+  accelerometerCharacteristicZ.writeValue(0);
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("BLE Accelerometer Peripheral");
+}
+
+void loop() {
+  BLEDevice central = BLE.central();
+
+  if (IMU.accelerationAvailable()) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    IMU.readAcceleration(x, y, z);
+
+    // we want to change the sampling rate by adding delay values here
+    accelerometerCharacteristicX.writeValue(x); 
+    /* will be using two different delay lengths to create a significant difference in the sampling rate
+    uncomment the delay line to test that specific sampling rate
+    my M4 code only looks at the x value for game controls */
+
+    //delay(100); // SAMPLING RATE 1
+
+    //delay(2); // SAMPLING RATE 2
+
+    accelerometerCharacteristicY.writeValue(y);
+    accelerometerCharacteristicZ.writeValue(z);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+}
